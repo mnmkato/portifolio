5187fbc8f6a88d81ee0173b0bf90264ecc572e09
mnmkato <katsmamal@gmail.com>
2023-09-26 21:18:57 +0300

    refined data and changed styling

--- script.js
+++ script.js
@@ -4,16 +4,55 @@ const data = {
     about:"I'm a passionate front-end developer with experience collaborating on diverse projects, where I've learnt different languages, frameworks and technologies. I'm committed to  performance optimization and  ensuring mobile responsiveness in projects I've done. Thank you for visiting my portfolio.",
     skills:[
         {
-            type: "Language",
-            list: ["JavaScript", "Python", "HTML/CSS"]
+            type: "Languages",
+            icon:"fa-code",
+            list:[
+                {
+                name:"JavaScript",
+                icon:"fa-square-js"
+                },
+                {
+                name:"Python",
+                icon:"fa-python"
+                },
+                {
+                name:"HTML",
+                icon:"fa-html5"
+                },
+                {
+                name:"CSS",
+                icon:"fa-css3-alt"
+                },]
         },
         {
             type: "Frameworks",
-            list: ["React", "NodeJS"]
+            icon:"fa-book-open-reader",
+            list:[
+                {
+                name:"React",
+                icon:"fa-react"
+                },
+                {
+                name:"NodeJs",
+                icon:"fa-node-js"
+                },]
         },
         {
             type: "Technologies",
-            list: ["Git", "Webpack", "Vite"]
+            icon:"fa-gear",
+            list:[
+                {
+                name:"Git",
+                icon:"fa-git-alt"
+                },
+                {
+                name:"Webpack",
+                icon:"fa"
+                },
+                {
+                name:"Vite",
+                icon:"fa"
+                },]
         }],
     projectList:[
         {
@@ -56,18 +95,27 @@ function generateSkills() {
     const skillsContainer = document.getElementById("skills_content");
     
     data.skills.forEach(skillCategory => {
-        const categoryTitle = document.createElement("h3");
+        const icon = document.createElement("i");
+        icon.classList.add("fa-solid",skillCategory.icon);
+        
+        const categoryTitle = document.createElement("h2");
         categoryTitle.textContent = skillCategory.type;
 
         const skillsList = document.createElement("ul");
         skillCategory.list.forEach(skill => {
             const skillItem = document.createElement("li");
-            skillItem.textContent = skill;
+            skillItem.textContent = skill.name;
+
+            const icon = document.createElement("i");
+            icon.classList.add("fa-brands",skill.icon);
+    
             skillsList.appendChild(skillItem);
+            skillItem.appendChild(icon);
         });
 
         const categoryContainer = document.createElement("div");
         categoryContainer.classList.add("skill-category");
+        categoryContainer.appendChild(icon);
         categoryContainer.appendChild(categoryTitle);
         categoryContainer.appendChild(skillsList);
 
